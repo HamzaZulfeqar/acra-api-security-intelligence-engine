1153f7f34ac2e980c2fb31f88820ffedf6978515
HamzaZulfeqar <302222421+HamzaZulfeqar@users.noreply.github.com>
2026-09-24 23:36:19 +0500

    Add Sprint 11 OAuth context dimensions

---
+++ core/src/main/java/io/acra/core/oauth/OAuthContextDimension.java
@@ -0,0 +1,14 @@
+package io.acra.core.oauth;
+
+public enum OAuthContextDimension {
+    ISSUER,
+    REDIRECT_URI,
+    RESOURCE,
+    AUDIENCE,
+    SCOPE,
+    RESPONSE_TYPE,
+    GRANT_TYPE,
+    PKCE,
+    STATE_SIGNAL,
+    NONCE_SIGNAL
+}
