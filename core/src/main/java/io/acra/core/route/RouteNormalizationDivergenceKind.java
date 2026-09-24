b91054c6a0e0d4db9569353f182e04d2781bfe8d
HamzaZulfeqar <302222421+HamzaZulfeqar@users.noreply.github.com>
2026-09-24 10:49:37 +0500

    Add Sprint 8 route normalization domain

---
+++ core/src/main/java/io/acra/core/route/RouteNormalizationDivergenceKind.java
@@ -0,0 +1,9 @@
+package io.acra.core.route;
+
+public enum RouteNormalizationDivergenceKind {
+    NONE,
+    REPRESENTATION_CHANGE,
+    FAMILY_VARIATION,
+    CANONICAL_DIVERGENCE,
+    INCONCLUSIVE
+}
