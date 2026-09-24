0455b7e692c232d61fa2ecf789b110ceb5f4daeb
HamzaZulfeqar <302222421+HamzaZulfeqar@users.noreply.github.com>
2026-09-25 01:23:22 +0500

    Verify Sprint 10 reporting exports

--- scripts/verify-sprint10-foundation.sh
+++ scripts/verify-sprint10-foundation.sh
@@ -60,6 +60,7 @@ java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10ControlledBatchExecutionT
 java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10ControlledIndirectExecutionTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10FindingCandidateProjectionTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10CoverageAccountingTestSuite
+java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10BatchIndirectReportingExportTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint9.Sprint9PropertyAuthorizationFoundationTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint8.Sprint8RoutingNormalizationFoundationTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint7.Sprint7WorkflowAuthorizationFoundationTestSuite
