16169367c3e7c3b179a6d8541dfc88698fde6b81
HamzaZulfeqar <302222421+HamzaZulfeqar@users.noreply.github.com>
2026-09-25 00:36:00 +0500

    Verify Sprint 10 coverage accounting

--- scripts/verify-sprint10-foundation.sh
+++ scripts/verify-sprint10-foundation.sh
@@ -59,6 +59,7 @@ java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10ControlledBatchIndirectLa
 java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10ControlledBatchExecutionTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10ControlledIndirectExecutionTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10FindingCandidateProjectionTestSuite
+java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10CoverageAccountingTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint9.Sprint9PropertyAuthorizationFoundationTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint8.Sprint8RoutingNormalizationFoundationTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint7.Sprint7WorkflowAuthorizationFoundationTestSuite
