98bcca104dddbf11cf92ce0850d31dd3aa79c133
HamzaZulfeqar <302222421+HamzaZulfeqar@users.noreply.github.com>
2026-09-24 23:44:21 +0500

    Verify Sprint 11 passive OAuth extraction

--- scripts/verify-sprint11-foundation.sh
+++ scripts/verify-sprint11-foundation.sh
@@ -16,6 +16,7 @@ javac --release 21 -Xlint:all -Werror -cp "$BUILD/main" -d "$BUILD/test" @"$BUIL
 CP="$BUILD/main:$BUILD/test"
 java -ea -cp "$CP" io.acra.core.tests.sprint11.Sprint11OAuthOidcFoundationTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint11.Sprint11OAuthEvidenceBindingTestSuite
+java -ea -cp "$CP" io.acra.core.tests.sprint11.Sprint11PassiveOAuthExtractionTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10SessionContextFoundationTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint9.Sprint9PropertyAuthorizationFoundationTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint8.Sprint8RoutingNormalizationFoundationTestSuite
