6ae158a46c50663e43910041abeaeaa62a433b2a
HamzaZulfeqar <302222421+HamzaZulfeqar@users.noreply.github.com>
2026-09-24 22:14:33 +0500

    Verify Sprint 10 session security assessment

--- scripts/verify-sprint10-foundation.sh
+++ scripts/verify-sprint10-foundation.sh
@@ -56,6 +56,7 @@ java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10SessionContextFoundationT
 java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10SessionEvidenceBindingTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10ControlledSessionContextTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10PassiveSessionHydrationTestSuite
+java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10SessionSecurityAssessmentTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint9.Sprint9PropertyAuthorizationFoundationTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint8.Sprint8RoutingNormalizationFoundationTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint7.Sprint7WorkflowAuthorizationFoundationTestSuite
