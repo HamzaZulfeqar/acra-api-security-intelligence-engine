dc3c081bf033b14afcea3185c3d54fb241be1772
HamzaZulfeqar <302222421+HamzaZulfeqar@users.noreply.github.com>
2026-09-25 01:53:08 +0500

    Verify Sprint 11 prediction adapters

--- scripts/verify-sprint11-foundation.sh
+++ scripts/verify-sprint11-foundation.sh
@@ -65,6 +65,7 @@ PY
 CP="$BUILD/main:$BUILD/test"
 java -ea -cp "$CP" io.acra.core.tests.sprint11.Sprint11ResearchAblationFoundationTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint11.Sprint11EvaluationDatasetManifestTestSuite
+java -ea -cp "$CP" io.acra.core.tests.sprint11.Sprint11AblationPredictionAdapterTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint10.Sprint10BatchIndirectFoundationTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint9.Sprint9PropertyAuthorizationFoundationTestSuite
 java -ea -cp "$CP" io.acra.core.tests.sprint8.Sprint8RoutingNormalizationFoundationTestSuite
